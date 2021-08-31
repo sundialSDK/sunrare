6bd387f54b2ed2ae92964c4c25f72944918462b8
Alen Korbut <alenkorbut@gmail.com>
2021-08-31 14:06:08 +0300

    fixed SwiftPM locations

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "SunRareSP",
+    name: "SunRare",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SunRareSP",
-            targets: ["SunRareSP"]),
+            name: "SunRare",
+            targets: ["SunRare"]),
     ],
     targets: [
         .binaryTarget(
-                    name: "SunRareSP",
+                    name: "SunRare",
                     path: "SunRare.xcframework"
                 )
     ]
